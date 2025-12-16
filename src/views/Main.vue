60a269f0ba0f8f4d86024ee6b2dcfe6f5ccd90a9
Menmery <2053186460@qq.com>
2025-12-16 15:44:45 +0800

    element-plus按需引入

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,11 @@
 <template>
   <div>layout</div>
+  <el-button round>Round</el-button>
+  <el-button type="primary" round>Primary</el-button>
+  <el-button type="success" round>Success</el-button>
+  <el-button type="info" round>Info</el-button>
+  <el-button type="warning" round>Warning</el-button>
+  <el-button type="danger" round>Danger</el-button>
 </template>
 
 <script setup></script>
